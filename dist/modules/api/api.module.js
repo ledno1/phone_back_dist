9e73d5b3d5b7e683f5b318e6ace019c8dc8e0b19
mac <99mahuateng2023@gmail.com>
2023-09-05 00:28:30 +0800

    快速更新

--- dist/modules/api/api.module.js
+++ dist/modules/api/api.module.js
@@ -31,7 +31,6 @@ const aLiPayHandler_service_1 = require("./subHandler/aLiPayHandler.service");
 const handlerTemplate_service_1 = require("./subHandler/handlerTemplate.service");
 const top_temp_entity_1 = require("../../entities/order/top_temp.entity");
 const XiaoMangProxyChargingHandlerservice_1 = require("./subHandler/XiaoMangProxyChargingHandlerservice");
-const paycode_service_1 = require("../payCode/paycode.service");
 let ApiModule = class ApiModule {
 };
 ApiModule = __decorate([
@@ -62,7 +61,7 @@ ApiModule = __decorate([
             ws_module_1.WSModule
         ],
         controllers: [orderSell_controller_1.OrderSellController, orderTop_controller_1.OrderTopController, api_controller_1.ApiController],
-        providers: [orderSell_service_1.OrderSellService, orderTop_service_1.OrderTopService, api_service_1.ApiService, orderHandler_service_1.orderConsumer, proxyChargingAPI_service_1.ProxyChargingAPIService, wxChannelAPI_service_1.WxChannelAPIService, aLiPayHandler_service_1.ALiPayHandlerService, handlerTemplate_service_1.HandlerTemplateService, XiaoMangProxyChargingHandlerservice_1.XiaoMangProxyChargingHandlerservice, paycode_service_1.PayCodeService],
+        providers: [orderSell_service_1.OrderSellService, orderTop_service_1.OrderTopService, api_service_1.ApiService, orderHandler_service_1.orderConsumer, proxyChargingAPI_service_1.ProxyChargingAPIService, wxChannelAPI_service_1.WxChannelAPIService, aLiPayHandler_service_1.ALiPayHandlerService, handlerTemplate_service_1.HandlerTemplateService, XiaoMangProxyChargingHandlerservice_1.XiaoMangProxyChargingHandlerservice],
         exports: [orderSell_service_1.OrderSellService, orderTop_service_1.OrderTopService, api_service_1.ApiService]
     })
 ], ApiModule);
